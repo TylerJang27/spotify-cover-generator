4d875fd6fde5f2686fabcf8d64f31af8b4cf0a4a
Tyler Jang <tylerjang27@gmail.com>
2025-04-29 00:00:45 -0700

    get track list

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spotify Cover Generator
+
+This is a simple command-line tool to generate Spotify cover photos for playlists.
+
+Repo documentation TODO
---
+++ src/main.ts
@@ -0,0 +1,101 @@
+import { Command } from "commander";
+import "dotenv/config";
+import { Playlist, PlaylistedTrack, SpotifyApi } from "@spotify/web-api-ts-sdk";
+
+const MAX_PLAYLIST_PAGES = 5;
+const PLAYLIST_PAGE_SIZE = 50;
+const MAX_PLAYLIST_TRACK_PAGES = 10;
+const PLAYLIST_TRACK_PAGE_SIZE = 50;
+
+const getSdk = () => {
+  const clientId = process.env.SPOTIFY_CLIENT_ID;
+  const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+  if (!clientId || !clientSecret) {
+    throw new Error("SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET must be set");
+  }
+
+  return SpotifyApi.withClientCredentials(clientId, clientSecret);
+};
+
+const findPlaylist = async (userId: string, playlistName: string) => {
+  const sdk = getSdk();
+  let total = 0;
+  const regex = new RegExp(playlistName);
+
+  for (let i = 0; i < MAX_PLAYLIST_PAGES; i++) {
+    // Playlists are sorted by recency of creation
+    const playlistsResult = await sdk.playlists.getUsersPlaylists(
+      userId,
+      PLAYLIST_PAGE_SIZE,
+      i * PLAYLIST_PAGE_SIZE,
+    );
+    total = playlistsResult.total;
+    const playlist = playlistsResult.items.filter(
+      ({ name }) => name.includes(playlistName) || regex.test(name),
+    );
+    if (playlist.length) {
+      return playlist[0];
+    }
+  }
+  throw new Error(
+    `Playlist not found matching \`${playlistName}\`. Searched ${Math.min(total, MAX_PLAYLIST_PAGES * PLAYLIST_PAGE_SIZE)} of ${total} playlists`,
+  );
+};
+
+const listTracks = async (playlist: Playlist): Promise<PlaylistedTrack[]> => {
+  const sdk = getSdk();
+  const tracks = [];
+  let total = 0;
+
+  for (let i = 0; i < MAX_PLAYLIST_TRACK_PAGES; i++) {
+    // "items(added_by.id,track(name,href,album(name,href)))"
+    const pagedTracks = await sdk.playlists.getPlaylistItems(
+      playlist.id,
+      undefined,
+      "items(track(album(name), artists, id, name, popularity, uri))",
+      PLAYLIST_TRACK_PAGE_SIZE,
+      i * PLAYLIST_TRACK_PAGE_SIZE,
+    );
+    tracks.push(...pagedTracks.items);
+    total = pagedTracks.total;
+    if (total < MAX_PLAYLIST_TRACK_PAGES * (i + 1)) {
+      break;
+    }
+  }
+  if (total > tracks.length) {
+    console.log(`Truncated playlist size from ${total} to ${tracks.length}`);
+  }
+  return tracks;
+};
+
+const query = async (userId: string, playlistName: string) => {
+  const playlist = await findPlaylist(userId, playlistName);
+  const tracks = await listTracks(playlist);
+  console.log(
+    `Retrieved ${tracks.length} tracks from playlist \`${playlist.name}\``,
+  );
+
+  console.log(tracks);
+};
+
+const main = () => {
+  const program = new Command();
+
+  program
+    .name("spotify-cover-generator")
+    .description("CLI to query Spotify and generate playlist images")
+    .version("0.1.0");
+  program
+    .command("query")
+    .argument(
+      "<userId>",
+      "userId to search for playlists, usually alphanumeric",
+    )
+    // TODO(Tyler): Make playlist names optional
+    .argument("<playlistRegex>", "The name/regex of the playlist to evaluate")
+    .action(query);
+
+  program.parse(process.argv);
+};
+
+main();
